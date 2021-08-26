87208e4901e112debd67b4ed9f3c47ca29560a36
unknown <anshurockingboy23@gmail.com>
2021-08-26 15:10:06 +0530

    deployment

--- db_con.php
+++ db_con.php
@@ -1,4 +1,4 @@
-<?php 
-	$con=mysqli_connect("localhost","root","","project");
-
-?>
+<?php
+// development part
+// $con=mysqli_connect("localhost","root","","project");
+$con = mysqli_connect("remotemysql.com", "URWkM7xgH9", "4znhcx3j9O", "URWkM7xgH9");
